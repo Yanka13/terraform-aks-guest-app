a687e42a0fdbe9a9225318d8e158d90c119cd85d
Yannis Bouhamou <61419311+Yanka13@users.noreply.github.com>
2024-01-10 22:22:19 +0100

    Update kubernetes.tf

--- infra/kubernetes.tf
+++ infra/kubernetes.tf
@@ -4,7 +4,7 @@ resource "azurerm_kubernetes_cluster" "default" {
   location            = var.location
   resource_group_name = var.resource_group_name
   dns_prefix          = "${var.resource_group_name}-k8s"
-  kubernetes_version  = "1.26.3"
+  kubernetes_version  = "1.26.6"
 
   default_node_pool {
     name            = "default"
